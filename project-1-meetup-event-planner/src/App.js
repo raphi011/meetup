cb57a4aa5501531ff6487753101b3875264319d8
Raphael Gruber <raphi011@gmail.com>
2016-09-22 16:04:38 +0200

    adds boilerplate code

---
+++ project-1-meetup-event-planner/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './app.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>Welcome to Reactttssss</h2>
+        </div>
+        <p>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ project-1-meetup-event-planner/webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: "style!css"
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loaders: ["style", "css", "sass"]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
